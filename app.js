38d29ad6d2af871b02ed94511590cdb382dc1fae
Pierre Parel <pirparel03@gmail.com>
2022-07-13 22:12:31 +0800

    Add color picker and change view ratio.

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 
 const container = document.querySelector('.container');
 const createButn = document.querySelector('.createNewCanvas');
+const canvas = document.querySelector('.container')
+const changeColor = document.querySelectorAll('.color');
+
+let selectedColor = 'black';
+
+changeColor.forEach(btn => btn.addEventListener('click', (e) => {
+    console.log(btn);
+    console.log(btn.getAttribute('id'));
+    let newColor = btn.getAttribute('id');
+    switch(newColor){
+        case "red":
+            selectedColor = "#FF4D4D";
+            break;
+        case "blue":
+            selectedColor = "#1638FF";
+        break;
+        case "yellow":
+            selectedColor = "#FFD837";
+        break;
+        case "green":
+            selectedColor = "#009B21";
+        break;
+        case "white":
+            selectedColor = "white";
+        break;
+        case "erase":
+            selectedColor = "black";
+        break;
+    }
+}));
 
 
+canvas.addEventListener('mousedown', e => {
+    if (e.buttons == 1){
+        window.addEventListener('mouseover', (eve) => {
+            paintPix(eve,selectedColor);
+        });
+        window.addEventListener('mousedown', (eve) => {
+            paintPix(eve,selectedColor);
+        });
+    }
+    else{
+        return;
+    }
+});
 
 createButn.addEventListener('click', () => {
     let num;
@@ -18,11 +61,20 @@ createButn.addEventListener('click', () => {
     createCanvas(num);
 });
 
+function paintPix(e, color){
+    if(e.buttons == 1){
+        if(e.target.classList == 'pix'){
+            let pix = e.target;
+            pix.style.backgroundColor = color;
+        }
+    }
+}
+
 function createCanvas(num){    
 // Create 16x16 Canvas of div
     let row = ""
     for(let i = 0; i < num; i++){
-        row += "<div class='pix'></div>";
+        row += "<div class='pix' draggable='false'></div>";
     }
     for(let i = 0; i < num; i++){
         let box = document.createElement('div');
@@ -30,12 +82,16 @@ function createCanvas(num){
         box.innerHTML = row;
         container.appendChild(box);
     }
-    const pixs = document.querySelectorAll('.pix');
-    pixs.forEach(pix => pix.addEventListener('click', () =>{
-        pix.style.backgroundColor = "white"
-    }));
+
 }
 
+
+
+// Paint function
+// const pixs = document.querySelectorAll('.pix');
+// pixs.forEach(pix => pix.addEventListener('click', () =>{
+//     pix.style.backgroundColor = "white"
+// }));
 createCanvas(16);
 
 
